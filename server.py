f4a5e5a80a05866e38ff32b9eca0121d130369bc
maciek367 <maciek2baba2@gmail.com>
2023-04-22 22:34:18 +0200

    add db.json, add route to write to it and route to get messages from it

--- server.py
+++ server.py
@@ -1,11 +1,24 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
+with open("db.json", "r") as f:
+    data = json.load(f)
 
-@app.route("/api/hello")
-def hello():
-    return {"message": "Hello, World!"}
+messages = data["messages"]
+
+
+@app.route("/messages", methods=["POST"])
+def write_message(message):
+    messages.append(message)
+    with open("db.json", "w") as f:
+        json.dump(data, f)
+
+
+@app.route("/messages", methods=["GET"])
+def get_messages():
+    return messages
 
 
 if __name__ == "__main__":
